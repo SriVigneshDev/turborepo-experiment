ac1a070ab438cd620a6051db85db3dfb00753eac
Sri Vignesh <srivignesh.p@enfec.com>
2025-12-03 15:48:57 +0530

    feat: fixed error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
+        <Button type="button" appName="web" className={styles.secondary}>
           Open alert
         </Button>
       </main>
